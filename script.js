66bd1452f5b37097c07056a0a9b1ef3c0e7c9cd7
D_00 <69427207+d-002@users.noreply.github.com>
2023-09-21 12:36:54 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -12,7 +12,7 @@ function deleteAllCookies() {
   var cookies = document.cookie.split(";");
 
   for (var i = 0; i < cookies.length; i++) {
-    document.cookie = cookies[i].split("=")[0] + "=; expires=Thu, 01 Jan 1970 00:00:00 GMT; path=/";
+    document.cookie = cookies[i].split("=")[0] + "=; expires=Thu, 01 Jan 1970 00:00:00 GMT; path=/jstolist";
   }
 }
 
