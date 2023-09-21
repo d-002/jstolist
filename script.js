21b85c4d3dcd627872256298bde5cf8faa627be7
d-002 <minecraftenjoy@outlook.fr>
2023-09-21 13:54:20 +0200

    test

--- script.js
+++ script.js
@@ -1,20 +1,8 @@
-// TODO: store in cookies, don't show cookie popup again, same for help (show on first use)
-
 let divs = [];
 let todo = {0: [], 1: [], 2: [], 3: []};
 let selectdiv, helpdiv, footerdiv;
 
-let date = new Date(Date.now() + 5184000000); // expires in 60 days
-let cookieEnd = "; expires=" + date.toGMTString()
-let preferences = {};
-
-function deleteAllCookies() {
-  var cookies = document.cookie.split(";");
-
-  for (var i = 0; i < cookies.length; i++) {
-    document.cookie = cookies[i].split("=")[0] + "=; expires=Thu, 01 Jan 1970 00:00:00 GMT";
-  }
-}
+let saved = true;
 
 function _export() {
 	
@@ -24,31 +12,34 @@ function _import() {
 	
 }
 
+function update() {
+	if (!saved) {
+		for (let i = 0; i < 4; i++) {
+			let children = divs[i].children;
+			let string = "";
+			for (let j = 0; j < children.length-1; j++) {
+				string += children[j].children[0].value.replace("\t", "    ");
+				if (j < children.length-2) {
+					string += "\t";
+				}
+			}
+			localStorage.setItem(i, string);
+		}
+
+		saved = true;
+		lastSave = Date.now();
+		console.log("Saved to local storage");
+	}
+}
+
 function showHelp() {
 	helpdiv.style.display = "block";
 }
 
 function hideHelp() {
+	preferences["helpok"] = 1;
+	localStorage.setItem("helpok", 1);
 	helpdiv.style = "";
-}
-
-function readCookies() {
-	let cookie = document.cookie.split("; ");
-	for (let i = 0; i < cookie.length; i++) {
-		let key = cookie[i].split("=")[0];
-		let value = cookie[i].split("=")[1];
-		preferences[key] = value;
-	}
-
-	if (preferences["cookieok"] == "1") {
-		hideFooter();
-	}
-}
-
-function hideFooter() {
-	document.cookie = "cookiesok=1"+cookieEnd;
-	console.log("cookiesok=1"+cookieEnd);
-	footerdiv.style.display = "none";
 }
 
 function add(i) {
@@ -64,6 +55,7 @@ function add(i) {
 function remove(i, elt) {
 	elt.parentNode.removeChild(elt);
 	todo[i].splice(todo[i].indexOf(elt), 1);
+	saved = false;
 }
 
 function init() {
@@ -73,7 +65,8 @@ function init() {
 	helpdiv = document.getElementById("help");
 	footerdiv = document.getElementById("footer");
 
-	readCookies();
+	document.body.addEventListener("keyup", () => {saved = false});
+	console.log(localStorage);
 
 	let text = ["More urgent, less important - <strong>DELEGATE</strong>",
 				"Less urgent, less important - <strong>DELETE/MOVE</strong>",
@@ -90,4 +83,6 @@ function init() {
 		}
 		divs.push(div.lastChild);
 	}
+
+	window.setInterval(update, 5000);
 }
